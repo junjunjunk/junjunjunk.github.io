522f08a484b7a5b3095a2ca9897b34f458844758
junjunjunk <juntrp0207@gmail.com>
2021-04-05 22:44:57 +0900

    [Modify] syntax error

--- next.config.js
+++ next.config.js
@@ -3,9 +3,5 @@ require('dotenv').config();
 module.exports = {
     poweredByHeader: false,
     reactStrictMode: true,
-
-    module.exports = {
-        assetPrefix: process.env.GITHUB_PAGES ? '/junjunjunk.github.io' : '',
-    };
-
+    assetPrefix: process.env.GITHUB_PAGES ? '/junjunjunk.github.io' : '',
 }
