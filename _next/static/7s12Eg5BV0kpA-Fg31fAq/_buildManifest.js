a867d119f3680710ed9a7af1ea5ae045fab44ad6
junjunjunk <junjunjunk@users.noreply.github.com>
2021-04-11 15:00:32 +0000

    deploy: 4d956632e23860a043100467af93f4035776c334

--- _next/static/7s12Eg5BV0kpA-Fg31fAq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ffdc8792da13741a1bc3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d9c04cfceb534f9fd581.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
